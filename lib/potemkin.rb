d5feb004cc7323fe597f3ad50fc4baf1e58d3cb1
metapsj <patrickjohnson@forty3sixteen.com>
2014-03-01 20:21:00 -0800

    refactor potemkin methods into dsl.rb

--- lib/potemkin.rb
+++ lib/potemkin.rb
@@ -1,27 +1,12 @@
 require_relative 'potemkin/application'
 require_relative 'potemkin/server'
 require_relative 'potemkin/responder'
+require_relative 'potemkin/dsl'
 
 if ARGV.size > 0
   services_path = ARGV.shift
 else
   services_path = './config/services.rb'
-end
-
-def port(p)
-  server = Server.new p
-  Application.instance.add_server server
-end
-
-def respond(config, &block)
-  resource = config[:resource]
-
-  h = Responder.new(resource) do |request, response|
-    response['Content-Type'] = 'text/xml'
-    block.call(request, response) 
-  end
-
-  Application.instance.servers.last.add_responder h
 end
 
 load services_path
