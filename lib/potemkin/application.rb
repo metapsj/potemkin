b1ad40780478dee4a7ee5fb99ff5a6edba64a9be
metapsj <patrickjohnson@forty3sixteen.com>
2014-04-15 10:13:18 -0700

    touched application.rb nothing signaficant

--- lib/potemkin/application.rb
+++ lib/potemkin/application.rb
@@ -5,16 +5,16 @@ module Potemkin
 
   class Application
     attr_reader :script_path, :pool
-
+    
     def initialize(argv)
       @pool = Pool.instance
 
       @script_path = argv.size > 0 ? argv.shift : './example/services.rb'
     end
-
+    
     def run
       script = File.read(@script_path)
-      
+
       self.instance_eval script, @script_path
 
       @pool.start
